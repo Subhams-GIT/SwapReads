3324a3c0f875ae15bf19cb3c44effd8bf78230cd
Subhams-GIT <subhamkumardas4444@gmail.com>
2024-05-12 20:36:46 +0530

    username showing after logging in and logout will show only if logged in

--- loginPage.js
+++ loginPage.js
@@ -15,7 +15,6 @@
 	if(sessionStorage.getItem('user-info'))
 		{
 			let logout=document.getElementById("logout");
-			console.log(logout);
 			logout.removeAttribute('style');
 		}
  })
@@ -26,14 +25,15 @@
 
  // Your web app's Firebase configuration
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ 
  const firebaseConfig = {
-	apiKey: "AIzaSyBEiIKrS7Raxv-73DyTjv_7xHkPKCyTKoo",
-	authDomain: "gssoc-issue-solve.firebaseapp.com",
-	databaseURL: "https://gssoc-issue-solve-default-rtdb.firebaseio.com",
-	projectId: "gssoc-issue-solve",
-	storageBucket: "gssoc-issue-solve.appspot.com",
-	messagingSenderId: "897320160719",
-	appId: "1:897320160719:web:7cb31eed6e903af62bd6ba"
+	apiKey: "AIzaSyCu2WXknNce_49J5BLuR1DyHm319hu6dWc",
+	authDomain: "login-13127.firebaseapp.com",
+	projectId: "login-13127",
+	storageBucket: "login-13127.appspot.com",
+	messagingSenderId: "151656578300",
+	appId: "1:151656578300:web:c67208ab6ec437c844ab79",
+	measurementId: "G-L3PPD58MZ8",
   };
 
  // Initialize Firebase
@@ -44,7 +44,7 @@
  //console.log(app);
  const auth = getAuth();
 
-
+ 
  
  //----- New Registration code start
  let register = document.querySelector(".register");
@@ -99,7 +99,6 @@
 	 })
 	email="";
 	password="";
-	console.log(userCredential._tokenResponse.email);
 	document.getElementById("logout").style.display = "block";
 	//window.location.href = "index.html";
    } 
